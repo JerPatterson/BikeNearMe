53989f1fba6501242ee742ba0b83ecc9b4aa57dc
JerPatterson <jeremie.patterson@polymtl.ca>
2024-05-19 14:48:49 -0400

    fix dependencies (move them from dev-dependencies)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,13 +1,14 @@
-// ignore_for_file: depend_on_referenced_packages
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:latlong2/latlong.dart';
 
+
 const minZoom = 10.0;
 const maxZoom = 20.0;
 const initialZoom = 14.0;
 const initialCenter = LatLng(45.504789, -73.613187);
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
