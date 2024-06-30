d597c233d71661ca28a731f72b88f27f4ea09ba3
JerPatterson <jeremie.patterson@polymtl.ca>
2024-06-30 11:16:18 -0400

    remove unecessary imports

--- lib/pages/station_info.dart
+++ lib/pages/station_info.dart
@@ -1,9 +1,7 @@
 import 'package:bike_near_me/entities/station_information.dart';
 import 'package:bike_near_me/entities/station_status.dart';
 import 'package:bike_near_me/icons/bike_share.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:latlong2/latlong.dart';
